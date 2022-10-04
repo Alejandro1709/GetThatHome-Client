9a39d615d9bb6cadb1c144e4707463770d2ab5ee
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-04 11:18:43 -0500

    Add: new file to components/PropertyList.js

---
+++ src/components/PropertyList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropertyCardDetail from './PropertyCardDetail';
+import styled from '@emotion/styled';
+
+const StyledList = styled.div`
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 1rem;
+  row-gap: 2rem;
+`;
+
+function PropertyList() {
+  return (
+    <StyledList>
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+      <PropertyCardDetail />
+    </StyledList>
+  );
+}
+
+export default PropertyList;
