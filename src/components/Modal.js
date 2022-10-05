2f08201dcaf8a9023288ccc17b6b183fc4aef53d
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-05 12:24:19 -0500

    Add: position fixed to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,11 +5,11 @@ const StyledModal = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  right: 0;
+  bottom: 0;
   background-color: rgba(0, 0, 0, 0.25);
   z-index: 100;
 `;
