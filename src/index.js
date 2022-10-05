da834545a13b6a05874cbafc67606908dff3743b
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-05 11:00:08 -0500

    Add: Modal to components/

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Global } from "@emotion/react";
-import { global, reset } from "./styles";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PropertiesPage from "./pages/PropertiesPage";
-import SignupPage from "./pages/SignupPage";
-import LandingPage from "./components/LandingPage";
-import PropertyDetailPage from "./pages/property-detail-page";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
+import { global, reset } from './styles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PropertiesPage from './pages/PropertiesPage';
+import SignupPage from './pages/SignupPage';
+import LandingPage from './components/LandingPage';
+import PropertyDetailPage from './pages/property-detail-page';
+import Modal from './components/Modal';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Global styles={global} />
     <Global styles={reset} />
     <BrowserRouter>
+      <Modal>ldofo</Modal>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/properties" element={<PropertiesPage />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/properties' element={<PropertiesPage />} />
         {/* For the route property detail page add the id of the property */}
-        <Route path="/properties/1" element={<PropertyDetailPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path='/properties/1' element={<PropertyDetailPage />} />
+        <Route path='/signup' element={<SignupPage />} />
         <Route
-          path="/myproperties"
+          path='/myproperties'
           element={<h1>Landlord Properties Page</h1>}
         />
-        <Route path="/saved" element={<h1>Homeseeker Properties Page</h1>} />
-        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path='/saved' element={<h1>Homeseeker Properties Page</h1>} />
+        <Route path='*' element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
