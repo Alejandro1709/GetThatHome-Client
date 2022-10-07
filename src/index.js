b19e70c960f1afc833e58d5a4c992a4d62e3a8a5
Bryan <mgbryan90@hotmail.com>
2022-10-07 16:05:30 -0500

    Change: single quotes by double quotes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./context/auth-context";
@@ -6,16 +5,8 @@ import { Global } from "@emotion/react";
 import { global, reset } from "./styles";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Global } from '@emotion/react';
-import { global, reset } from './styles';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
->>>>>>> develop
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Global styles={global} />
