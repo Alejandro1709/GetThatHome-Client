15b6d3da6c9f7e27057b7c2b8599d830272feaae
David Montoya <aaronmontoya360@gmail.com>
2022-10-08 11:18:30 -0500

    Add: merge develop into david

--- src/pages/PropertiesPage.js
+++ src/pages/PropertiesPage.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
-import PropertyList from '../components/PropertyList';
-import SorteableBar from '../components/SorteableBar';
-import PaginationBar from '../components/PaginationBar';
-import { useProperties } from '../context/properties-context';
-import styled from '@emotion/styled';
+import { useEffect, useState } from "react";
+import PropertyList from "../components/PropertyList";
+import SorteableBar from "../components/SorteableBar";
+import PaginationBar from "../components/PaginationBar";
+import { useProperties } from "../context/properties-context";
+import styled from "@emotion/styled";
+import { filterProperties } from "../components/button-group/utils";
 
 const StyledContainer = styled.div`
   max-width: 1200px;
@@ -18,16 +19,29 @@ const PropertiesContainer = styled.div`
   justify-content: space-between;
 `;
 
+const initialFilters = {
+  price: { min: 0, max: Infinity },
+  type: { apartments: null, houses: null },
+  ambients: { beds: 0, baths: 0 },
+  pets: null,
+  area: { min: 0, max: Infinity },
+  op_type: { rent: null, sale: null },
+};
+
 function PropertiesPage() {
   const { propsByPreferences } = useProperties();
+  const [filters, setFilters] = useState(initialFilters);
+  const filteredProps = filterProperties(properties, filters);
 
   const [filtered, setFiltered] = useState(propsByPreferences);
 
   return (
     <StyledContainer>
-      <SorteableBar setFiltered={setFiltered} />
+      <SorteableBar filters={filters} setFilters={setFilters} />
+      {/* <SorteableBar setFiltered={setFiltered} /> */}
       <PropertiesContainer>
-        <PropertyList properties={filtered}/>
+        {/* <PropertyList properties={filtered} /> */}
+        <PropertyList properties={filteredProps} />
         <PaginationBar />
       </PropertiesContainer>
     </StyledContainer>
