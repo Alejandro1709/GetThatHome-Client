4ed98fcad4370159563c8649ae72dbac3b9480c0
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-24 17:38:23 -0500

    Add: marker to MapBox component

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,8 +1,10 @@
 // import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
-import Map from "react-map-gl";
+import Map, { Marker } from "react-map-gl";
+import {IoIosPin} from "react-icons/io";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useState } from "react";
 import LoadingWave from "./LoadingWave";
+import { colors } from "../styles";
 
 export default function MapBox({ coordValues }) {
   const [loading, setLoading] = useState(true);
@@ -24,11 +26,15 @@ export default function MapBox({ coordValues }) {
           initialViewState={{
             latitude,
             longitude,
-            zoom: zoom || 10,
+            zoom: zoom || 15,
           }}
           style={{ width: "100%", height: "47.5rem", borderRadius: "1rem" }}
           mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
+        >
+          <Marker longitude={longitude} latitude={latitude} anchor="bottom">
+            <IoIosPin size="2rem" color={`${colors.primary[400]}`}/>
+          </Marker>
+        </Map>
       )}
     </>
   );
