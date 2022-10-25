fee0e62e126fc5cf3633096895deb4023feda107
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-24 20:40:19 -0500

    Move: Find a home button to Navbar component

--- src/components/UnAuthLayout.js
+++ src/components/UnAuthLayout.js
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import { typography } from "../styles/typography";
-import { FiSearch } from "react-icons/fi";
 import { RiUserReceivedLine } from "react-icons/ri";
 import { AiOutlineUserAdd } from "react-icons/ai";
 import { colors } from "../styles/colors";
@@ -9,14 +8,6 @@ import { NavLink } from "react-router-dom";
 const ButtonsNavBar = styled.div`
   display: flex;
   gap: 1rem;
-`;
-
-const FindHome = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0 1rem;
-  width: 10.5rem;
-  gap: 0.5rem;
 `;
 
 const ButtonJoin = styled.button`
@@ -60,43 +51,10 @@ const StyledNavLink = styled(NavLink)`
   align-items: center;
 `;
 
-const ButtonSearch = styled.p`
-  overflow: hidden;
-  border-right: 0.15em solid orange;
-  white-space: nowrap;
-  margin: 0 auto;
-  letter-spacing: 0.15em;
-  animation: typing 3.5s steps(30, end), blink-caret 0.5s step-end infinite;
-
-  @keyframes typing {
-    from {
-      width: 0;
-    }
-    to {
-      width: 100%;
-    }
-  }
-
-  @keyframes blink-caret {
-    from,
-    to {
-      border-color: transparent;
-    }
-    50% {
-      border-color: orange;
-    }
-  }
-`;
 
 function UnAuthLayout({ onLoginClick }) {
   return (
     <ButtonsNavBar>
-      <FindHome>
-        <StyledNavLink to="/properties">
-          <FiSearch />
-          <ButtonSearch>FIND A HOME</ButtonSearch>
-        </StyledNavLink>
-      </FindHome>
       <ButtonJoin>
         <StyledNavLink to="/signup">
           <AiOutlineUserAdd />
