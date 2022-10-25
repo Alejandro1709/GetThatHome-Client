1c30fb61f7f7cce8ddad9cac061ed0523ca0bcda
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-25 05:10:54 -0500

    Fix: map box bug when npm build

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,10 +1,17 @@
 // import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 import Map, { Marker } from "react-map-gl";
-import {IoIosPin} from "react-icons/io";
+import { IoIosPin } from "react-icons/io";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useState } from "react";
 import LoadingWave from "./LoadingWave";
 import { colors } from "../styles";
+import mapboxgl from 'mapbox-gl';
+
+    // The following is required to stop "npm build" from transpiling mapbox code.
+    // notice the exclamation point in the import.
+    // @ts-ignore
+    // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+    mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 export default function MapBox({ coordValues }) {
   const [loading, setLoading] = useState(true);
@@ -32,7 +39,7 @@ export default function MapBox({ coordValues }) {
           mapStyle="mapbox://styles/mapbox/streets-v9"
         >
           <Marker longitude={longitude} latitude={latitude} anchor="bottom">
-            <IoIosPin size="4rem" color={`${colors.primary[400]}`}/>
+            <IoIosPin size="4rem" color={`${colors.primary[400]}`} />
           </Marker>
         </Map>
       )}
